d8b82885ede298d3e93dc759b9aeda0bbcd808e4
emre-serdar <eserdar1@binghamton.edu>
2021-12-06 10:37:04 -0500

    Heads-up display is started to implement, health and timer labels are defined.

---
+++ BombermanClone/core/src/cs/binghamton/edu/Hud.java
@@ -0,0 +1,57 @@
+package cs.binghamton.edu;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public class Hud {
+    public Stage stage;
+    private Viewport viewport;
+    private Integer timer;
+    //private Integer score;
+    private Integer healthPoints;
+
+    Label countDown;
+    Label hp;
+    Label countDownLabel;
+    Label hpLabel;
+
+
+    Hud(SpriteBatch spriteBatch){
+        timer = 150;
+        //score = 0;
+
+        healthPoints = 100;
+
+        viewport = new FitViewport(Bomberman.WORLD_WIDTH,Bomberman.WORLD_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, spriteBatch);
+
+        //defining a table for labels
+        Table table = new Table();
+
+        //putting table to top of the screen
+        table.top();
+        table.setFillParent(true);
+
+        //creating labels
+        countDown = new Label(String.format("%03d", timer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        hp =  new Label(String.format("%03d", healthPoints), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        hpLabel = new Label("HEALTH", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        countDownLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        //inserting labels to table
+        table.add(hpLabel).expandX().padTop(10);
+        table.add(countDownLabel).expandX().pad(10);
+        table.row(); //defining a new raw after this line
+        table.add(hp).expandX();
+        table.add(countDown).expandX();
+
+
+    }
+}
